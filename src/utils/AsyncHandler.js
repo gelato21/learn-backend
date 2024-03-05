3d1e8b4c9a19b7b57646f57f2b20032fdf685b4a
pankaj <pkp20022@gmail.com>
2024-03-05 12:39:56 +0530

    util code

---
+++ src/utils/AsyncHandler.js
@@ -0,0 +1,26 @@
+
+const AsyncHandler = (requestHandler) => {
+    (req, res, next) => {   
+        Promise.resolve(requestHandler(req, res, next)).catch(err => next(err));
+    }
+}
+
+export {AsyncHandler}
+
+
+
+
+
+/*
+const AsyncHandler = (fn) => async(req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success:false,
+            message: error.message || "Internal Server Error" ,
+        })
+        next(error)
+    }
+}
+*/
